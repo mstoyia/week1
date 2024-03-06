971ff1277025f06579eb2b130516cd88325f0d0a
toyiajoseph <toyia0@gmail.com>
2024-03-06 10:36:13 -0500

    variable types

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+
+// access modifier
+public class HelloWorld {
+    public static void main(String[] args) {
+        //data type - name 
+           int   firstNumber;
+
+        firstNumber = 10;
+
+        int secondNumber;
+
+        secondNumber = 30;
+
+        int sum;
+
+        sum = firstNumber + secondNumber;
+
+        String name = "Joseph";
+
+        double price = 500.00;
+
+        //displaying or printing information
+        System.out.println(sum + name + price);
+
+    }
+}
